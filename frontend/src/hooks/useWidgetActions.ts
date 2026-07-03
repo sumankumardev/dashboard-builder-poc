990454d931bc78c2828928fcd7c9fe16d9663b03
Suman kumar <joewatson881178@gmail.com>
2026-07-03 17:02:47 +0530

    feat: update index.html title and meta description; refactor widget actions for delete and duplicate functionalities

--- frontend/src/hooks/useWidgetActions.ts
+++ frontend/src/hooks/useWidgetActions.ts
@@ -1,20 +1,39 @@
+import { dashboardApi } from "@/api/dashboard.api";
 import type { Widget } from "@/types/widget";
+import { useQuery } from "@tanstack/react-query";
 
-export const useWidgetActions = (widget: Widget) => {
-  const handleDelete = async () => {
-    
-  };
 
-  const handleDuplicate = async () => {};
 
-  const handleRefresh = () => {};
-
-  const handleEditSave = async () => {};
-
-  return {
-    handleDelete,
-    handleDuplicate,
-    handleRefresh,
-    handleEditSave,
-  };
+export const useWidgetDelete = (dashboardId: string, widgetId: string) => {
+  useQuery({
+    queryKey: ["widgetDelete"],
+    queryFn: async () => {
+      const res = await dashboardApi.deleteWidget(dashboardId, widgetId);
+      return res.data;
+    },
+  });
 };
+
+export const useWidgetDuplicate = (dashboardId: string, widgetId: string) => {
+  useQuery({
+    queryKey: ["widgetDuplicate"],
+    queryFn: async () => {
+      const res = await dashboardApi.duplicateWidget(dashboardId, widgetId);
+      return res.data;
+    },
+  });
+};
+
+//TODO
+
+
+// export const useWidgetEditSave=(dashboardId: string, widgetId: string,,)=>{
+//     useQuery({
+//         queryKey:["widgetEditSave"],
+//         queryFn:async ()=>{
+//             const res=await dashboardApi.updateWidget(dashboardId,widgetId),{
+//                 title:editTile
+//             };
+//         }
+//     })
+// }
